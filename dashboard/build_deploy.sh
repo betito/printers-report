bfda86390ee3a1d130d37e1f7583db9105da4a8f
betito.oliveira <betito.oliveira@gmail.com>
2022-05-15 00:42:00 -0400

    techinical imprivment in file attr for running on terminal.

--- dashboard/build_deploy.sh
+++ dashboard/build_deploy.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/bash
 
 echo "Building ...."
-ng build --prod --base-href ./
+ng build --base-href ./
 echo "Copy to /var/www/html ..."
 sudo cp -r dist/front /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/front
